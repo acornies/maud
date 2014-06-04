16318c749e2cc2896ada688d9d40a8f267e814c8
Andrew Cornies <acornies@kobo.com>
2014-06-04 10:41:35 -0400

    initial commit for legend peak: - basic player mechanics - basic camera movement - prototype meshes

---
+++ src/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+	
+	public float XMargin = 1.0f;
+	public float YMargin = 1.0f;
+	
+	public float XSmooth = 10.0f;
+	public float YSmooth = 10.0f;
+	
+	public Vector2 MaxXandY;
+	public Vector2 MinXandY;
+	
+	public Transform CameraTarget;
+	
+	void Awake ()
+	{
+		this.CameraTarget = GameObject.FindGameObjectWithTag("CameraTarget").transform;
+	}
+	
+	bool CheckXMargin()
+	{
+		return Mathf.Abs(transform.position.x - this.CameraTarget.position.x) > this.XMargin;
+	}
+	
+	bool CheckYMargin()
+	{
+		return Mathf.Abs(transform.position.y - this.CameraTarget.position.y) > this.YMargin;
+	}
+	
+	void FixedUpdate () {
+		this.TrackPlayer();
+	}
+	
+	void TrackPlayer()
+	{
+		var targetX = transform.position.x;
+		var targetY = transform.position.y;
+		
+		if (this.CheckXMargin())
+		{
+			targetX = Mathf.Lerp(transform.position.x, this.CameraTarget.position.x, this.XSmooth * Time.deltaTime);
+		}
+		
+		if (this.CheckYMargin())
+		{
+			targetY = Mathf.Lerp(transform.position.y, this.CameraTarget.position.y, this.YSmooth * Time.deltaTime);
+		}
+		
+		targetX = Mathf.Clamp(targetX, this.MinXandY.x, this.MaxXandY.x);
+		targetY = Mathf.Clamp(targetY, this.MinXandY.y, this.MaxXandY.y);
+		
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
+		
+	}
+}
---
+++ src/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Transform _tower;
+
+	public float MaxSpeed = 6.0f;
+	public bool FacingRight = true;
+	public float MoveDirection;
+
+	public float JumpSpeed = 600.0f;
+	public bool Grounded = false;
+	public float rotationSpeed = 10.0f;
+	public bool shouldRotate = true;
+	
+	void Awake()
+	{
+		//GroundCheck = GameObject.Find("GroundCheck").transform;
+		_tower = GameObject.Find("Tower").transform;
+	}
+	
+	// Use this for physics updates
+	void FixedUpdate ()
+	{
+		this.shouldRotate = true;
+		rigidbody.velocity = new Vector2(this.MoveDirection * this.MaxSpeed, rigidbody.velocity.y);
+
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.right, out hit, 0.1f)
+		    || Physics.Raycast(transform.position, Vector3.left, out hit, 0.1f)) {
+			Debug.Log("Hit " + hit.transform.gameObject.name);
+			if (hit.transform.tag == "Stoppable"){
+				this.shouldRotate = false;
+				rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+			}
+		}
+
+		Debug.DrawRay(transform.position, Vector3.right, Color.white);
+		Debug.DrawRay(transform.position, Vector3.left, Color.white);
+		
+		if (this.MoveDirection > 0.0f && !this.FacingRight)
+		{
+			Flip();
+		}
+		else if (this.MoveDirection < 0.0f && this.FacingRight)
+		{
+			Flip();
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		if (collision.gameObject.layer == 8)
+		{
+			this.Grounded = true;
+		}
+	}
+	
+	void OnCollisionStay(Collision collisionInfo) 
+	{
+		if (collisionInfo.gameObject.layer == 8){
+			this.Grounded = true;
+		}
+	}
+	
+	void OnCollisionExit(Collision collisionInfo) 
+	{
+		if (collisionInfo.gameObject.layer == 8){
+			this.Grounded = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		this.MoveDirection = Input.GetAxis("Horizontal");
+		
+		if (this.Grounded && Input.GetButtonDown("Jump"))
+		{
+			rigidbody.AddForce(new Vector2(0, JumpSpeed));
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow) && shouldRotate) {
+			_tower.Rotate(Vector3.up * this.rotationSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow) && shouldRotate) {
+			_tower.Rotate(-Vector3.up * this.rotationSpeed * Time.deltaTime);
+		}
+
+		//Debug.DrawRay(transform.position, Vector3.right, Color.white);
+
+		//Debug.Log(multiplier);
+	}
+	
+	void Flip()
+	{
+		this.FacingRight = !FacingRight;
+		transform.Rotate(Vector3.up, 180.0f, Space.World);
+	}
+}
