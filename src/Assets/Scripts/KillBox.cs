949985143d754c7a2eb1ae56dc9f45b753f722f2
Andrew Cornies <acornies@gmail.com>
2014-07-16 14:30:01 -0400

    Kill box update: - added new event for player death - moved kill box code to new KillBox.cs class

---
+++ src/Assets/Scripts/KillBox.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBox : MonoBehaviour {
+
+	public delegate void PlayerDeath();
+	public static event PlayerDeath On_PlayerDeath; 
+
+	// Subscribe to events
+	void OnEnable()
+	{
+		CameraMovement.On_CameraUpdatedMinY += UpdateKillBoxPosition;
+	}
+	
+	void OnDisable()
+	{
+		UnsubscribeEvent();
+	}
+	
+	void OnDestroy()
+	{
+		UnsubscribeEvent();
+	}
+	
+	void UnsubscribeEvent()
+	{
+		CameraMovement.On_CameraUpdatedMinY -= UpdateKillBoxPosition;
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.name == "Player") 
+		{
+			On_PlayerDeath();
+		}
+	}
+
+	void UpdateKillBoxPosition(float newYPosition)
+	{
+		Debug.Log ("New kill box position: " + newYPosition);
+		transform.position = new Vector3 (transform.position.x, newYPosition, transform.position.z);
+	}
+}
