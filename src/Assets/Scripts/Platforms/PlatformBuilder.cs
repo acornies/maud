7554f1e7a9e4390e7e8935cd21bba4b29d88dd01
Andrew Cornies <acornies@gmail.com>
2014-07-25 12:53:42 -0400

    PlatformBuilder behaviour: - changed prefab folder convention - added range info class to help introduce new platforms based on platform number

--- src/Assets/Scripts/Platforms/PlatformBuilder.cs
+++ src/Assets/Scripts/Platforms/PlatformBuilder.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,21 +11,35 @@ namespace LegendPeak.Platforms
 		/// <summary>
 		/// The _height level descriptions.
 		/// </summary>
-		private IDictionary<string, int> _heightLevelDescriptions;
+		private IDictionary<string, PlatformRangeInfo> _heightLevelDescriptions;
 
 		public PlatformBuilder()
 		{
-			_heightLevelDescriptions = new Dictionary<string, int> ();
-			_heightLevelDescriptions.Add (new KeyValuePair<string, int> ("Lowest", 100));
-			_heightLevelDescriptions.Add (new KeyValuePair<string, int> ("Lower", 200));
+			_heightLevelDescriptions = new Dictionary<string, PlatformRangeInfo> ();
+			_heightLevelDescriptions.Add (new KeyValuePair<string, PlatformRangeInfo> ("Lowest", new PlatformRangeInfo(20, 4)));
+			_heightLevelDescriptions.Add (new KeyValuePair<string, PlatformRangeInfo> ("Lower", new PlatformRangeInfo(40, 5)));
 		}
 
 		public string GetPlatformPrefabByNumber(int platformNumber)
 		{
-			var platformDescription = _heightLevelDescriptions.FirstOrDefault(i => i.Value >= platformNumber);
-			int range = Random.Range (1, 5);
-			string prefab = string.Format ("Prefabs/{0}/{1}_{2}", platformDescription.Key, range, platformDescription.Key);
+			var platformDescription = _heightLevelDescriptions.FirstOrDefault(i => i.Value.heightRange >= platformNumber);
+			if (platformDescription.Value != null)
+			{
+				return GetPrefab(platformDescription.Value.prefabRange);
+			}
+			// if there are no defined height levels, generate the last range forever
+			else
+			{
+				return GetPrefab(_heightLevelDescriptions.Last().Value.prefabRange);
+			}
+		}
+
+		private string GetPrefab(int prefabRange)
+		{
+			int range = Random.Range (1, prefabRange);
+			string prefab = string.Format ("Prefabs/Platforms/{0}_Platform", range);
 			return prefab;
 		}
+	
 	}
 }
