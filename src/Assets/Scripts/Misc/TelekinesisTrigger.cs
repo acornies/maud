14ff838e99c33204454acd54a3bcb627b69e1a88
Andrew Cornies <acornies@gmail.com>
2014-10-18 14:57:27 -0400

    adding tele trigger and effect for Gamercamp

---
+++ src/Assets/Scripts/Misc/TelekinesisTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TelekinesisTrigger : MonoBehaviour
+{
+
+    private TelekinesisController _telekinesisTrigger;
+    private ParticleSystem _aura;
+
+    // Use this for initialization
+    void Start()
+    {
+        _telekinesisTrigger = GameObject.Find("TelekinesisControl").GetComponent<TelekinesisController>();
+        _aura = transform.FindChild("TeleAura").GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.name + " tele: " + _telekinesisTrigger.enabled);
+        if (other.name != "Player" || _telekinesisTrigger.enabled) return;
+        //Debug.Log("Turn on tele.");
+        _telekinesisTrigger.enabled = true;
+        _aura.Play();
+        //Destroy(gameObject);
+    }
+}
