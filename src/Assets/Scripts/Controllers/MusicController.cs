48d71e79b3cffba54fd0358346e0c714be905ef7
Andrew Cornies <acornies@gmail.com>
2014-11-15 22:39:03 -0500

    Adding fade control to music controller. TODO: REFACTOR!!!

--- src/Assets/Scripts/Controllers/MusicController.cs
+++ src/Assets/Scripts/Controllers/MusicController.cs
@@ -70,10 +70,31 @@ public class MusicController : MonoBehaviour
     {
         var currentPlatform = PlatformController.Instance.GetCurrentPlatformNumber();
         NextSong(currentPlatform, forestMusicSlowLimit, forestMusicSlow, forestMusicFast);
-        NextSong(currentPlatform, forestMusicFastLimit, forestMusicFast, cloudMusicSlow);
+        FadeTransition(currentPlatform, forestMusicFastLimit, forestMusicFast, cloudMusicSlow);
         NextSong(currentPlatform, cloudMusicSlowLimit, cloudMusicSlow, cloudMusicFast);
         NextSong(currentPlatform, cloudMusicFastLimit, cloudMusicFast, stratosphereMusic);
     }
+
+	void FadeTransition(int currentPlatform, int limit, AudioSource current, AudioSource next)
+	{
+		if (currentPlatform > limit && current.isPlaying && !next.isPlaying) {
+			next.volume = 0f;
+			next.Play();
+			next.loop = true;
+		}
+
+		if (next.isPlaying && next.volume < 0.5f && current.isPlaying)
+		{
+			Debug.Log("Fade in: " + next.clip.name + " from: " + current.clip.name);
+			next.volume = Mathf.Lerp(next.volume, 0.5f, musicFadeSpeed * Time.deltaTime);
+			current.volume = Mathf.Lerp(current.volume, 0f, musicFadeSpeed * Time.deltaTime);
+
+			if (current.volume <= 0.2f)
+			{
+				current.Stop();
+			}
+		}
+	}
 
     private static void NextSong(int currentPlatform, int limit, AudioSource current, AudioSource next)
     {
@@ -91,7 +112,7 @@ public class MusicController : MonoBehaviour
     static void MusicTransition(AudioSource currentSong, AudioSource nextSong)
     {
         if (nextSong == null) return;
-        //Debug.Log("Start transition from " + currentSong.clip.name + " to " + nextSong.clip.name);
+        Debug.Log("Start transition from " + currentSong.clip.name + " to " + nextSong.clip.name);
         nextSong.Play();
         currentSong.volume = 0.0f;
         currentSong.Stop();
