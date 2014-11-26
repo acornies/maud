20aedb03ded2adc71b11567831eb1507eb392dd8
Andrew Cornies <acornies@gmail.com>
2014-11-26 14:31:10 -0500

    Start of power-up system.

---
+++ src/Assets/Scripts/Controllers/PowerUpController.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class PowerUpController : MonoBehaviour
+{
+    public int minPlatform = 1;
+    public float powerUpSpawnInterval = 1f;
+    [Range(-4f, 0)]
+    public float minXPosition = -1f;
+    [Range(0, 4f)]
+    public float maxXPosition = 1f;
+
+    public delegate void NewPowerUpLocation(Vector3 location);
+    public static event NewPowerUpLocation OnNewPowerUpLocation;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return StartCoroutine("SpawnPowerUps");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator SpawnPowerUps()
+    {
+        var levelPlatforms = PlatformController.Instance.levelPlatforms;
+
+        if (levelPlatforms == null || levelPlatforms.Count <= 0) yield return new WaitForSeconds(powerUpSpawnInterval);
+
+        if (PlatformController.Instance.GetCurrentPlatformNumber() < minPlatform)
+        {
+            Debug.Log("Hasn't reached min platform for pick-ups");
+            yield return new WaitForSeconds(powerUpSpawnInterval);
+        }
+        else
+        {
+            var bottomPlatform = levelPlatforms.Keys.Min();
+            var topPlatform = levelPlatforms.Keys.Max();
+            //var screenLeftToWorld = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f));
+
+            var newPosition = new Vector3(Random.Range(minXPosition, maxXPosition),
+                Random.Range(levelPlatforms[bottomPlatform].transform.position.y,
+                    levelPlatforms[topPlatform].transform.position.y),
+                    GameController.Instance.playerZPosition);
+
+            //Debug.Log("New pick-up position: " + newPosition);
+
+            if (OnNewPowerUpLocation != null)
+            {
+                OnNewPowerUpLocation(newPosition);
+            }
+
+            yield return new WaitForSeconds(powerUpSpawnInterval);
+        }
+
+    }
+}
--- src/Assets/Scripts/PowerUps/PowerUpBehaviour.cs
+++ src/Assets/Scripts/PowerUps/PowerUpBehaviour.cs
@@ -5,16 +5,40 @@ using System.Collections;
 public class PowerUpBehaviour : MonoBehaviour
 {
     private Animator _animator;
+    private ParticleSystem _particles;
 
     public float pickUpPower = 2f;
+    //public Vector3 newLocation = Vector2.zero;
 
     public delegate void PowerPickUp(float powerToAdd);
     public static event PowerPickUp OnPowerPickUp;
 
+    // Subscribe to events
+    void OnEnable()
+    {
+        PowerUpController.OnNewPowerUpLocation += HandleOnNewPowerUpLocation;
+    }
+
+    void OnDisable()
+    {
+        UnsubscribeEvent();
+    }
+
+    void OnDestroy()
+    {
+        UnsubscribeEvent();
+    }
+
+    void UnsubscribeEvent()
+    {
+        PowerUpController.OnNewPowerUpLocation -= HandleOnNewPowerUpLocation;
+    }
+
     // Use this for initialization
     private void Start()
     {
-        _animator = GetComponent<Animator>();
+        _animator = GetComponentInChildren<Animator>();
+        _particles = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -28,7 +52,7 @@ public class PowerUpBehaviour : MonoBehaviour
         if (otherCollider.name != "Player") return;
 
         _animator.enabled = false;
-        particleSystem.Stop();
+        _particles.Stop();
 
         if (OnPowerPickUp != null)
         {
@@ -39,6 +63,14 @@ public class PowerUpBehaviour : MonoBehaviour
     public void Reactivate()
     {
         _animator.enabled = true;
-        particleSystem.Play();
+        _particles.Play();
     }
+
+    private void HandleOnNewPowerUpLocation(Vector3 location)
+    {
+        Debug.Log("New pick-up position: " + location);
+        transform.position = location;
+        Reactivate();
+    }
+
 }
