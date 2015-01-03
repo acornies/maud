6ca128108bc70b48e74f37bd35bd253e1551894b
Andrew Cornies <acornies@gmail.com>
2015-01-02 20:34:33 -0500

    Adding directional light rotation with skybox, deleting protyping assets, adding real skyboxes

--- src/Assets/Scripts/Camera/SkyboxCameraMovement.cs
+++ src/Assets/Scripts/Camera/SkyboxCameraMovement.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class SkyboxCameraMovement : MonoBehaviour
 {
-    public static float speedMultiplier = 0.01f;
+    public Transform directionalLight;
+    public static float speedMultiplier = 0.05f;
     public float rotationSpeed = 1f;
+    public Skybox[] skyboxes;
+    public bool rotateLight;
+    public float maxRotationSpeed = 100f;
 
     void OnEnable()
     {
@@ -29,18 +33,26 @@ public class SkyboxCameraMovement : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        if (directionalLight == null)
+        {
+            Debug.LogError("No directional light set on Directional Light property of SkyboxCameraMovement.cs");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        if (rotateLight)
+        {
+            directionalLight.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+        
     }
 
     private void HandleOnMaxHeightIncrease(float amount)
     {
-        rotationSpeed = amount;
+        rotationSpeed = Mathf.Clamp(amount, 1f, maxRotationSpeed);
         //Debug.Log("Increased skybox rotation to: " + amount);
     }
 }
