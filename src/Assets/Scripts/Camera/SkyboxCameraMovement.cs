04d7df8d5e7ce62b2262b3727580849f75567bda
Andrew Cornies <acornies@gmail.com>
2015-01-04 00:18:36 -0500

    Fixed bug with player material/skybox transition into above clouds.

--- src/Assets/Scripts/Camera/SkyboxCameraMovement.cs
+++ src/Assets/Scripts/Camera/SkyboxCameraMovement.cs
@@ -5,26 +5,26 @@ using System.Collections;
 public class SkyboxCameraMovement : MonoBehaviour
 {
     private Light _sunlightComponent;
-    
+
     public Transform sunlightDirectionalLight;
-    public static float speedMultiplier = 0.02f;
+    public static float speedMultiplier = 0.015f;
     public float rotationSpeed = 1f;
     public Material[] skyboxes;
     public float maxRotationSpeed = 100f;
-	public float sunlightHorizontalRotation = 1f;
+    public float sunlightHorizontalRotation = 1f;
 
     public int dawnSkyboxTransitionPlatform = 61;
     public int cloudSkyboxTransitionPlatform = 150;
     public float transitionSpeed = 3f;
 
-	public Material playerCloudMaterial;
-	public Material playerAboveCloudMaterial;
+    public Material playerCloudMaterial;
+    public Material playerAboveCloudMaterial;
     public Color sunlightColorDawn;
     public Color sunlightColorAboveClouds;
     //public int aboveSkyboxTransitionPlatform = 150;
 
-	public delegate void PlayerMaterialUpdate(Material newMaterial);
-	public static event PlayerMaterialUpdate OnPlayerMaterialUpdate;
+    public delegate void PlayerMaterialUpdate(Material newMaterial);
+    public static event PlayerMaterialUpdate OnPlayerMaterialUpdate;
 
     void OnEnable()
     {
@@ -65,9 +65,9 @@ public class SkyboxCameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.Self);
         // rotate light opposite of skybox camera, x2 speed since light is rotating due to it's parent
-		//sunlightDirectionalLight.Rotate(new Vector3(0, -rotationSpeed * Time.deltaTime, 0), Space.Self);
+        //sunlightDirectionalLight.Rotate(new Vector3(0, -rotationSpeed * Time.deltaTime, 0), Space.Self);
 
         var currentPlatform = PlatformController.Instance.GetCurrentPlatformNumber();
         SkyboxTransition(0, currentPlatform, dawnSkyboxTransitionPlatform, sunlightColorAboveClouds, playerCloudMaterial);
@@ -75,7 +75,7 @@ public class SkyboxCameraMovement : MonoBehaviour
 
     }
 
-	void SkyboxTransition (int currentSkybox, int currentPlatform, int transitionPlatform, Color sunlightColor, Material playerMaterial)
+    void SkyboxTransition(int currentSkybox, int currentPlatform, int transitionPlatform, Color sunlightColor, Material playerMaterial)
     {
         if (currentPlatform < transitionPlatform) return;
 
@@ -84,23 +84,32 @@ public class SkyboxCameraMovement : MonoBehaviour
         var blend = Mathf.Lerp(RenderSettings.skybox.GetFloat("_Blend"), 1f, transitionSpeed * Time.deltaTime);
         RenderSettings.skybox.SetFloat("_Blend", blend);
 
-        _sunlightComponent.color = Color.Lerp(_sunlightComponent.color, sunlightColor, transitionSpeed*Time.deltaTime);
-		sunlightDirectionalLight.rotation = Quaternion.Lerp (sunlightDirectionalLight.rotation, Quaternion.Euler(40f, sunlightDirectionalLight.rotation.y, sunlightDirectionalLight.rotation.z), transitionSpeed * Time.deltaTime);
-		//sunlightDirectionalLight.localEulerAngles.y = 0;
-		//sunlightDirectionalLight.localEulerAngles.z = 0;
-		//Debug.Log ("Tilt sunlight rotation...");
+        _sunlightComponent.color = Color.Lerp(_sunlightComponent.color, sunlightColor, transitionSpeed * Time.deltaTime);
+
+        if (currentSkybox == 0)
+        {
+            sunlightDirectionalLight.rotation = Quaternion.Lerp(sunlightDirectionalLight.rotation, 
+                Quaternion.Euler(40f, sunlightDirectionalLight.rotation.y, sunlightDirectionalLight.rotation.z), 
+                transitionSpeed * Time.deltaTime);
+        }
+
+        //sunlightDirectionalLight.localEulerAngles.y = 0;
+        //sunlightDirectionalLight.localEulerAngles.z = 0;
+        //Debug.Log ("Tilt sunlight rotation...");
 
 
 
         var nextSkybox = (currentSkybox + 1);
-        if (!(RenderSettings.skybox.GetFloat("_Blend") >= 0.99f) || nextSkybox > (skyboxes.Length - 1)) return;
+        if (!(RenderSettings.skybox.GetFloat("_Blend") >= 0.99f)) return;
 
-		if (OnPlayerMaterialUpdate != null)
-		{
-			OnPlayerMaterialUpdate(playerMaterial);
-		}
+        if (OnPlayerMaterialUpdate != null)
+        {
+            OnPlayerMaterialUpdate(playerMaterial);
+        }
 
-       // Debug.Log("Set RenderSettings to " + nextSkybox + " skybox");
+        if (nextSkybox > (skyboxes.Length -1)) return;
+
+        Debug.Log("Set RenderSettings to " + nextSkybox + " skybox");
         RenderSettings.skybox = skyboxes[nextSkybox];
         RenderSettings.skybox.SetFloat("_Blend", 0);
     }
