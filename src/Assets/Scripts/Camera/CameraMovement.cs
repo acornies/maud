3736cbfa4ade8f39722249c726ddd3a7ff5e9cb6
Andrew Cornies <acornies@gmail.com>
2015-01-19 12:30:34 -0500

    Adding extruded platform model

--- src/Assets/Scripts/Camera/CameraMovement.cs
+++ src/Assets/Scripts/Camera/CameraMovement.cs
@@ -106,6 +106,7 @@ public class CameraMovement : MonoBehaviour
     private void HandleUpdateCameraSpeed(float speed)
     {
         //if (!(speed > YSmooth)) return;
+        if (_playerSuspended) return;
         YSmooth = speed;
         //Debug.Log("Match camera speed to: " + speed);
     }
