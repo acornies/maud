7051b32d4724eff517119ea79200596c5e5fa831
Andrew Cornies <acornies@gmail.com>
2015-03-06 12:55:07 -0800

    v1.1.0 WIP: - Updates to physics settings - Tilt threshold and sensitivity updates - cloud area now comes in at 60 platforms - update to initial camera zoom to game position - Player spawn position is now set to their highest platform if not destroyed

---
+++ src/Assets/Scripts/Player/DontGoThroughThings.js
@@ -0,0 +1,33 @@
+#pragma strict
+
+var layerMask : LayerMask; //make sure we aren't in this layer
+var skinWidth : float = 0.1; //probably doesn't need to be changed
+private var minimumExtent : float;
+private var partialExtent : float;
+private var sqrMinimumExtent : float;
+private var previousPosition : Vector3;
+private var myRigidbody : Rigidbody;
+private var playerCollider : Collider;
+//initialize values
+function Awake() {
+   myRigidbody = rigidbody;
+   previousPosition = myRigidbody.position;
+   playerCollider = transform.FindChild("PlayerModel").GetComponent(Collider);
+   minimumExtent = Mathf.Min(Mathf.Min(playerCollider.bounds.extents.x, playerCollider.bounds.extents.y), playerCollider.bounds.extents.z);
+   partialExtent = minimumExtent*(1.0 - skinWidth);
+   sqrMinimumExtent = minimumExtent*minimumExtent;
+}
+
+function FixedUpdate() {
+   //have we moved more than our minimum extent?
+   var movementThisStep : Vector3 = myRigidbody.position - previousPosition;
+   var movementSqrMagnitude : float = movementThisStep.sqrMagnitude;
+   if (movementSqrMagnitude > sqrMinimumExtent) {
+      var movementMagnitude : float = Mathf.Sqrt(movementSqrMagnitude);
+      var hitInfo : RaycastHit;
+      //check for obstructions we might have missed
+      if (Physics.Raycast(previousPosition, movementThisStep, hitInfo, movementMagnitude, layerMask.value))
+         myRigidbody.position = hitInfo.point - (movementThisStep/movementMagnitude)*partialExtent;
+   }
+   previousPosition = myRigidbody.position;
+}
